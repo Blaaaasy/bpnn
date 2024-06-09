972c1e2a0fc1399e542d3e13455389fff8ef86d8
Blasy <Blaaaasy@users.noreply.github.com>
2024-06-09 23:00:51 +0800

    1

--- bpnn.py
+++ bpnn.py
@@ -2,7 +2,8 @@
 
 import math
 import random
-#import string
+import pandas as pd
+# import string
 try:
     import cPickle as pickle
 except:
@@ -11,16 +12,23 @@ except:
 random.seed(0)
 
 # calculate a random number where:  a <= rand < b
+
+
 def rand(a, b):
     return (b-a)*random.random() + a
 
 # our sigmoid function, tanh is a little nicer than the standard 1/(1+e^-x)
+
+
 def sigmoid(x):
     return math.tanh(x)
 
 # derivative of our sigmoid function, in terms of the output (i.e. y)
+
+
 def dsigmoid(y):
     return 1.0 - y**2
+
 
 class Unit:
     def __init__(self, length):
@@ -28,19 +36,25 @@ class Unit:
         self.change = [0.0] * length
         self.threshold = rand(-0.2, 0.2)
         #self.change_threshold = 0.0
+
     def calc(self, sample):
         self.sample = sample[:]
-        partsum = sum([i * j for i, j in zip(self.sample, self.weight)]) - self.threshold
+        partsum = sum(
+            [i * j for i, j in zip(self.sample, self.weight)]) - self.threshold
         self.output = sigmoid(partsum)
         return self.output
+
     def update(self, diff, rate=0.5, factor=0.1):
-        change = [rate * x * diff + factor * c for x, c in zip(self.sample, self.change)]
+        change = [rate * x * diff + factor * c for x,
+                  c in zip(self.sample, self.change)]
         self.weight = [w + c for w, c in zip(self.weight, change)]
         self.change = [x * diff for x in self.sample]
         #self.threshold = rateN * factor + rateM * self.change_threshold + self.threshold
         #self.change_threshold = factor
+
     def get_weight(self):
         return self.weight[:]
+
     def set_weight(self, weight):
         self.weight = weight[:]
 
@@ -50,31 +64,35 @@ class Layer:
         self.units = [Unit(input_length) for i in range(output_length)]
         self.output = [0.0] * output_length
         self.ilen = input_length
+
     def calc(self, sample):
         self.output = [unit.calc(sample) for unit in self.units]
         return self.output[:]
+
     def update(self, diffs, rate=0.5, factor=0.1):
         for diff, unit in zip(diffs, self.units):
             unit.update(diff, rate, factor)
+
     def get_error(self, deltas):
         def _error(deltas, j):
             return sum([delta * unit.weight[j] for delta, unit in zip(deltas, self.units)])
-        return [_error(deltas, j) for j  in range(self.ilen)]
+        return [_error(deltas, j) for j in range(self.ilen)]
+
     def get_weights(self):
         weights = {}
         for key, unit in enumerate(self.units):
             weights[key] = unit.get_weight()
         return weights
+
     def set_weights(self, weights):
         for key, unit in enumerate(self.units):
             unit.set_weight(weights[key])
 
 
-
 class BPNNet:
     def __init__(self, ni, nh, no):
         # number of input, hidden, and output nodes
-        self.ni = ni + 1 # +1 for bias node
+        self.ni = ni + 1  # +1 for bias node
         self.nh = nh
         self.no = no
         self.hlayer = Layer(self.ni, self.nh)
@@ -92,19 +110,19 @@ class BPNNet:
         # output activations
         self.ao = self.olayer.calc(self.ah)
 
-
         return self.ao[:]
-
 
     def update(self, targets, rate, factor):
         if len(targets) != self.no:
             raise ValueError('wrong number of target values')
 
         # calculate error terms for output
-        output_deltas = [dsigmoid(ao) * (target - ao) for target, ao in zip(targets, self.ao)]
+        output_deltas = [dsigmoid(ao) * (target - ao)
+                         for target, ao in zip(targets, self.ao)]
 
         # calculate error terms for hidden
-        hidden_deltas = [dsigmoid(ah) * error for ah, error in zip(self.ah, self.olayer.get_error(output_deltas))]
+        hidden_deltas = [dsigmoid(
+            ah) * error for ah, error in zip(self.ah, self.olayer.get_error(output_deltas))]
 
         # update output weights
         self.olayer.update(output_deltas, rate, factor)
@@ -113,7 +131,6 @@ class BPNNet:
         self.hlayer.update(hidden_deltas, rate, factor)
         # calculate error
         return sum([0.5 * (t-o)**2 for t, o in zip(targets, self.ao)])
-
 
     def test(self, patterns):
         for p in patterns:
@@ -131,29 +148,36 @@ class BPNNet:
                 error = error + self.update(targets, N, M)
             if i % 100 == 0:
                 print('error %-.10f' % error)
-            #self.save_weights('tmp.weights')
+            # self.save_weights('tmp.weights')
+
     def save_weights(self, fn):
         weights = {
-                "olayer":self.olayer.get_weights(),
-                "hlayer":self.hlayer.get_weights()
-                }
+            "olayer": self.olayer.get_weights(),
+            "hlayer": self.hlayer.get_weights()
+        }
         with open(fn, "wb") as f:
             pickle.dump(weights, f)
+
     def load_weights(self, fn):
-            with open(fn, "rb") as f:
-                weights = pickle.load(f)
-                self.olayer.set_weights(weights["olayer"])
-                self.hlayer.set_weights(weights["hlayer"])
+        with open(fn, "rb") as f:
+            weights = pickle.load(f)
+            self.olayer.set_weights(weights["olayer"])
+            self.hlayer.set_weights(weights["hlayer"])
+
 
 def demo():
     # Teach network XOR function
-    pat = [
-        [[0,0], [0]],
-        [[0,1], [1]],
-        [[1,0], [1]],
-        [[1,1], [0]]
-    ]
+    # pat = [
+    #     [[0, 0], [0]],
+    #     [[0, 1], [1]],
+    #     [[1, 0], [1]],
+    #     [[1, 1], [0]]
+    # ]
 
+    # 读取CSV文件
+    df = pd.read_csv('Tianchi_Train_uid_iid_rt.csv')
+    pat = [[[user_id, click_article_id], [rating]] for user_id, click_article_id,
+           rating in zip(df['user_id'], df['click_article_id'], df['ratings'])]
     # create a network with two input, two hidden, and one output nodes
     n = BPNNet(2, 2, 1)
     # train it with some patterns
@@ -161,8 +185,10 @@ def demo():
     # test it
     n.save_weights("demo.weights")
 
-    n.test(pat)
-
+    df2 = pd.read_csv('Tianchi_Train_uid_iid.csv')
+    pat2 = [[[user_id, click_article_id], [rating]] for user_id, click_article_id,
+            rating in zip(df2['user_id'], df2['click_article_id'], df2['ratings'])]
+    n.test(pat2)
 
 
 if __name__ == '__main__':
